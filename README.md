0777dce8425f791b8d5f34faa3330734e9766da3
cronugs <cronugs@outlook.com>
2020-01-10 11:35:19 +0100

    created model for user profile

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
 
 https://github.com/stefanfoulis/django-phonenumber-field
+
+creating a user profile using a one-to-one link
+https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone = PhoneNumberField()
+    email = models.EmailField()
+    personnummer = models.CharField(max_length=12)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    consent = models.BooleanField()
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.onjects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
