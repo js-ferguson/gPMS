2f04220f8e4fc030cbf961bae0fdc0222339256f
cronugs <cronugs@outlook.com>
2020-01-27 20:54:33 +0100

    created a js function that checks the url and executes the appropriate funciton to create a new map

--- static/js/maps.js
+++ static/js/maps.js
@@ -26,4 +26,27 @@ function initMap() {
 
 var latlng = JSON.parse(document.getElementById('latlng-data').textContent);
 
+function clinicListing() {
+    ref = "http://localhost:5000/clinic_listing/";
+    console.log(window.location.href.slice(22, -1));
+    if (window.location.href.slice(22, -1) === "clinic_listing") {
+
+        listing_map = new google.maps.Map(document.getElementById('listing-map'), {
+            zoom: 10
+
+        });
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                initialLocation = new google.maps.LatLng(position.coords.latitude,
+                                                         position.coords.longitude);
+                listing_map.setCenter(initialLocation);
+            }); 
+
+        }
+
+
+    } 
+}
+clinicListing();
+
 console.log(latlng);
