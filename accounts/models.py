710f1f19b5275c8966f97446a6f4289f332084f4
Jimi Windmills <jimi@cortex>
2020-02-03 09:14:23 +0100

    added password for local psql db at work

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,19 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 # from django.db.models.signals import post_save
 # from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
+
 from clinic.models import Clinic
 
 User = get_user_model()
 
 
 class Modalities(models.Model):
-     name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
-     def __str__(self):
-         return self.name
+    def __str__(self):
+        return self.name
 
 
 class Profile(models.Model):
@@ -34,14 +35,12 @@ class Profile(models.Model):
 
     def save(self):
         super().save()
- 
 
 
 # @receiver(post_save, sender=User)
 # def create_profile(sender, instance, created, **kwargs):
 #     if created:
 #         Profile.objects.create(user=instance)
-
 
 # @receiver(post_save, sender=User)
 # def save_profile(sender, instance, **kwargs):
