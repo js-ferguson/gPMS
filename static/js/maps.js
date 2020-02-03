9c0a8e716cb853acc5ddf10b2ee8ed3ddc50f06b
James Ferguson <jimi.windmills@gmail.com>
2020-02-03 14:47:52 +0100

    removed unneeded comments and cleaned up maps.jsw

--- static/js/maps.js
+++ static/js/maps.js
@@ -9,6 +9,16 @@ function loadScript() {
 
 var map;
 
+function setCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var initialLocation = new google.maps.LatLng(position.coords.latitude,
+                                                         position.coords.longitude);
+            map.setCenter(initialLocation);
+        }); 
+    }
+}
+
 function placeMarkers(latlng) {
     for (i = 0; i <= latlng.length; i++) {
         var obj = latlng[i];
@@ -23,13 +33,12 @@ function placeMarkers(latlng) {
 }
 
 function mapSelector() {
+    setCurrentLocation();
     if (window.location.href.slice(22, -1) === "clinic_listing") {
         var latlng = JSON.parse(document.getElementById('latlng-data').textContent);
-        console.log(latlng);
         map = new google.maps.Map(document.getElementById('listing-map'), {
             zoom: 10
         });
-        setCurrentLocation();
         placeMarkers(latlng);
 
     } else {
@@ -37,61 +46,7 @@ function mapSelector() {
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 10
         });
-        setCurrentLocation();
         placeMarkers(latlng);
     }
 }
 
-function setCurrentLocation() {
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var initialLocation = new google.maps.LatLng(position.coords.latitude,
-                                                         position.coords.longitude);
-            console.log(initialLocation.toString());
-            console.log(position);
-            map.setCenter(initialLocation);
-        }); 
-     }
-}
-
-
-/* function initMap() {
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude,
-                                                     position.coords.longitude);
-            map.setCenter(initialLocation);
-        }); 
-
-    }
-}
-*/
-//var listing_map;
-
-
-
-/*function clinicListing() {
-    ref = "http://localhost:5000/clinic_listing/";
-    console.log(window.location.href.slice(22, -1));
-    if (window.location.href.slice(22, -1) === "clinic_listing") {
-        
-        listing_map = new google.maps.Map(document.getElementById('listing-map'), {
-            zoom: 10
-
-        });
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                initialLocation = new google.maps.LatLng(position.coords.latitude,
-                                                         position.coords.longitude);
-                listing_map.setCenter(initialLocation);
-            }); 
-
-        }
-
-
-    } 
-}
-clinicListing();
-*/
