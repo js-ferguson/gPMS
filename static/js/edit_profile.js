f55f99806bf2cf0e870af53d5032426394107b3e
James Ferguson <jimi.windmills@gmail.com>
2020-03-13 14:45:56 +0100

    Pactitioners can edit their user details, profile and clinic profile from their profile page

---
+++ static/js/edit_profile.js
@@ -0,0 +1,15 @@
+function editProfile() {
+    const element = document.querySelector('#user-profile');
+    element.style.display = 'none';
+
+    const form = document.querySelector('#user-profile-form');
+    form.style.display = 'inline-block';
+}
+
+function editClinic() {
+    const element = document.querySelector('#clinic-profile');
+    element.style.display = 'none';
+
+    const form = document.querySelector('#clinic-profile-form');
+    form.style.display = 'inline-block';
+}
--- static/js/reviews.js
+++
@@ -1,8 +0,0 @@
-console.log("this is the review");
-
-//$(document).ready( function() {
-
-//    $("#edit_review_button").click( function(event) {
-//        {{ views.edit_review() }};
-//    });
-//});
